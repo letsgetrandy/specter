40ab9cd69786b65bb92722104f3bc068249d1014
Randy Hunt <letsgetrandy@gmail.com>
2013-11-06 18:15:56 -0600

    more efficient in-page behavior

--- demo/test-tumblr.js
+++ demo/test-tumblr.js
@@ -2,32 +2,26 @@
 
 open("https://tumblr.com/", function() {
 
-    // first, hide the background image before capturing
-    perform(function() {
+    test([1024, 768, 640, 320], function() {
+        // first, hide the background image before capturing
         var $ = window.jQuery;
         $('#fullscreen_post_bg').hide();
-    });
-
-    // then, capture a couple of elements at four screen widths
-    test([1024, 768, 640, 320], function() {
+        // then, capture a couple of elements at four screen widths
         capture(".dash_b_form_header", "form_header");
         capture(".dash_b_form", "login_form");
     });
 
 
     // FOR DEMO ONLY:
-
+    // run the tests again with changes, to demonstrate failures
+    test([1024, 768, 640, 320], function() {
         // now we can show the background to cause a test failure
-        perform(function() {
-            var $ = window.jQuery;
-            $('#fullscreen_post_bg').show();
-        });
-
+        var $ = window.jQuery;
+        $('#fullscreen_post_bg').show();
         // recapturing those elements should cause test failures
-        test([1024, 768, 640, 320], function() {
-            capture(".dash_b_form_header", "form_header");
-            capture(".dash_b_form", "login_form");
-        });
+        capture(".dash_b_form_header", "form_header");
+        capture(".dash_b_form", "login_form");
+    });
 
     // always call finish() at the end
     finish();
